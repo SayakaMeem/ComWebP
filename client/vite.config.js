4f5aea126a389ab4e3917ff898eca386f4538f86
SayakaMeem <sayakaalam419@gmail.com>
2026-09-21 02:09:51 +0600

    fix: vercel deployment - client build config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    outOfDir: true
+  },
+  server: {
+    port: 5175,
+    host: true
+  }
 })
